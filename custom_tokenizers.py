b6738f00654ced3e5ca9e6dfc7771f360dc7ba77
Kheims <djamelmesbah141@gmail.com>
2024-10-02 12:05:28 +0200

    fix issues

--- custom_tokenizers.py
+++ custom_tokenizers.py
@@ -3,6 +3,17 @@ import re
 import torch
 from transformers import RobertaTokenizer, RobertaModel
 
+MAX_LEN = 4000
+
+def encode_snippet(token_to_idx, snippet):
+        return [token_to_idx.get(token, token_to_idx["<UNK>"]) for token in snippet]
+
+def pad_sequence(seq, max_len, pad_idx):
+    if len(seq) < max_len:
+        return seq + [pad_idx] * (max_len - len(seq))  # Padding with 0
+    else:
+        return seq[:max_len]  # Truncate to max_len
+
 
 class TokenizerStrategy(ABC):
     @abstractmethod
@@ -14,12 +25,55 @@ class TokenizerStrategy(ABC):
         pass
 
 class SimpleTokenizer(TokenizerStrategy):
+    def __init__(self):
+        self.vocab = None
+        self.vocab_size = None 
+
     def tokenize(self, text):
         tokens = re.findall(r'\w+|\S', text)  # Basic split by non-alphanumeric chars
         return tokens 
+    def build_vocab(self, tokenized_data):
+        self.vocab = set(token for snippet in tokenized_data for token in snippet)
+        self.vocab.add("<UNK>")
+        self.vocab.add("<PAD>")
+        self.vocab_size = len(self.vocab)
+        return self.vocab_size
+
+    def get_vocab_size(self):
+        return self.vocab_size
+    
+    def get_preprocessed_data(self, tokens):
+        
+        if self.vocab is None:
+            self.build_vocab(tokens)
+
+        token_to_idx = {token: idx for idx, token in enumerate(self.vocab)}
+        pad_idx = token_to_idx["<PAD>"]
+
+        encoded_data = [encode_snippet(token_to_idx, snippet) for snippet in tokens]
+    
+        padded_data = [pad_sequence(seq, MAX_LEN, pad_idx) for seq in encoded_data]
+
+        return padded_data
+    
+    def get_collate_fn(self):
+        def simple_collate_fn(batch):
+            
+            inputs, labels = zip(*batch)
+            # Convert each input (which is a list) into a tensor
+            inputs_tensor = [torch.tensor(input_seq, dtype=torch.long) for input_seq in inputs]
+        
+            # Pad sequences to have the same length within the batch if necessary
+            inputs_tensor = torch.nn.utils.rnn.pad_sequence(inputs_tensor, batch_first=True, padding_value=0)
+        
+            # Convert labels into a tensor
+            labels_tensor = torch.tensor(labels, dtype=torch.long)
+        
+            return inputs_tensor, labels_tensor
+        return simple_collate_fn
+    
     def provides_embeddings(self):
         return False
-
 
 class BertTokenizer(TokenizerStrategy):
     def __init__(self):
@@ -27,16 +81,46 @@ class BertTokenizer(TokenizerStrategy):
         self.model = RobertaModel.from_pretrained("microsoft/codebert-base")
 
     def tokenize(self, text):
-        return self.tokenizer.encode(text, add_special_tokens=True, padding='max_length', truncation=True, max_length=512)
+        tokens = self.tokenizer.encode_plus(
+            text, 
+            add_special_tokens=True, 
+            padding='max_length', 
+            truncation=True, 
+            max_length=512,
+            return_tensors='pt'
+        )
+        return tokens
     
+    def get_preprocessed_data(self, tokens):
+        return tokens
+
     def get_embeddings(self, tokenized_input):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        tokenized_input = tokenized_input.to(device)
+        input_ids = tokenized_input["input_ids"].to(device)
+        attention_mask = tokenized_input["attention_mask"].to(device)
         self.model.to(device)
 
         with torch.no_grad():
-            outputs = self.model(tokenized_input)
+            outputs = self.model(tokenized_input, attention_mask=attention_mask)
         return outputs.last_hidden_state  # CodeBERT embeddings
+
+    def get_vocab_size(self):
+        return self.tokenizer.vocab_size    
+
+    def get_collate_fn(self):
+            
+        def bert_collate_fn(batch):
+            input_ids = [item[0] for item in batch]
+            attention_masks = [item[1] for item in batch]
+            labels = [item[2] for item in batch]
+            
+            input_ids = torch.nn.utils.rnn.pad_sequence(input_ids, batch_first=True, padding_value=0)
+            attention_masks = torch.nn.utils.rnn.pad_sequence(attention_masks, batch_first=True, padding_value=0)
+            labels = torch.tensor(labels, dtype=torch.long)
+
+            return {'input_ids': input_ids, 'attention_mask': attention_masks}, labels
+        
+        return bert_collate_fn
 
     def provides_embeddings(self):
         return True
@@ -50,3 +134,11 @@ class TokenizerContext:
 
     def tokenize(self, text):
         return self.strategy.tokenize(text)
+    
+    def get_preprocessed_data(self, tokens):
+        return self.strategy.get_preprocessed_data(tokens)
+    
+    def get_vocab_size(self):
+        return self.strategy.get_vocab_size()
+    def get_collate_fn(self):
+        return self.strategy.get_collate_fn()
--- train.py
+++ train.py
@@ -1,69 +1,68 @@
 import torch.nn as nn
 import torch.optim as optim
-import json
 import torch
+import json
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_recall_fscore_support, classification_report
-from torch.utils.data import DataLoader, Dataset
+from sklearn.utils.class_weight import compute_class_weight
+from imblearn.over_sampling import RandomOverSampler
+from torch.utils.data import DataLoader, Dataset, WeightedRandomSampler
+import numpy as np
 from time import time 
 import re
+import logging
 
 from model import LSTMModel, LSTMConfig, LSTMMLCQ
 from config import (LSTMConfig, OptimizerConfig, SchedulerConfig, TrainingConfig, 
                     get_optimizer, get_scheduler)
 from custom_tokenizers import SimpleTokenizer, BertTokenizer, TokenizerContext
 
+
+logging.basicConfig(
+    filename='training_log.txt',
+    filemode='a',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 # ----------------------------- Data Prep -------------------------------------------------------------
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 with open("MLCQCodeSmellSamples.json", "r") as f:
     data = json.load(f)
 
-codebert_tokenizer = BertTokenizer()
-simple_tokenizer = SimpleTokenizer()
 
 
-tokenizer_context = TokenizerContext(codebert_tokenizer)
+tokenizer_context = TokenizerContext(SimpleTokenizer())
 
 tokenized_data = [tokenizer_context.tokenize(entry["code_snippet"]) for entry in data]
 
+
 label_encoder = LabelEncoder()
-labels = [entry["smell"] for entry in data]
+labels = [entry["smell"] if entry["severity"] != "none" else "no_smell" for entry in data]
 encoded_labels = label_encoder.fit_transform(labels)  
 
 
 
-## Create vocabulary from tokenized data
-#vocab = set(token for snippet in tokenized_data for token in snippet)
-#vocab.add("<UNK>")
-#vocab.add("<PAD>")
-#vocab_size = len(vocab)
-#token_to_idx = {token: idx for idx, token in enumerate(vocab)}
-#pad_idx = token_to_idx["<PAD>"]
-#
-## Convert tokenized data to indexed sequences
-#def encode_snippet(snippet):
-#    return [token_to_idx.get(token, token_to_idx["<UNK>"]) for token in snippet]
-#
-#encoded_data = [encode_snippet(snippet) for snippet in tokenized_data]
-#
-## Padding and truncating to a fixed length
-#max_len = 1000  
-#def pad_sequence(seq, max_len):
-#    if len(seq) < max_len:
-#        return seq + [pad_idx] * (max_len - len(seq))  # Padding with 0
-#    else:
-#        return seq[:max_len]  # Truncate to max_len
-#
-#padded_data = [pad_sequence(seq, max_len) for seq in encoded_data]
 
-X_train, X_test, y_train, y_test = train_test_split(tokenized_data, encoded_labels, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(tokenizer_context.get_preprocessed_data(tokenized_data),
+                                                     encoded_labels, test_size=0.2, random_state=42)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+y_train_tensor = torch.tensor(y_train, dtype=torch.long)
+unique_classes = torch.unique(y_train_tensor)
+class_sample_count = torch.tensor([torch.sum(y_train_tensor == t).item() for t in unique_classes])
+weight = 1.0 / class_sample_count.double()
 
-X_train_tensor = torch.tensor(X_train, dtype=torch.long).to(device)
-X_test_tensor = torch.tensor(X_test, dtype=torch.long).to(device)
-y_train_tensor = torch.tensor(y_train, dtype=torch.long).to(device)
-y_test_tensor = torch.tensor(y_test, dtype=torch.long).to(device)
+samples_weight = weight[y_train_tensor]
+
+samples_weight = samples_weight.double()
+
+sampler = WeightedRandomSampler(samples_weight, len(samples_weight))
+
+class_weights = compute_class_weight(class_weight='balanced', classes=np.unique(y_train), y=y_train)
+
+class_weights_tensor = torch.tensor(class_weights, dtype=torch.float).to(device)
+
 
 class CodeSmellDataset(Dataset):
     def __init__(self, X, y):
@@ -75,13 +74,30 @@ class CodeSmellDataset(Dataset):
 
     def __getitem__(self, idx):
         return self.X[idx], self.y[idx]
+    
+
+class CodeSmellDatasetBert(Dataset):
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        tokenized_input = self.X[idx]
+        input_ids = tokenized_input['input_ids'].squeeze(0)  # Shape [seq_len]
+        attention_mask = tokenized_input['attention_mask'].squeeze(0)  # Shape [seq_len]
+        return input_ids, attention_mask, self.y[idx]
+    
 
 
-train_dataset = CodeSmellDataset(X_train_tensor, y_train_tensor)
-test_dataset = CodeSmellDataset(X_test_tensor, y_test_tensor)
 
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+train_dataset = CodeSmellDataset(X_train, y_train)
+test_dataset = CodeSmellDataset(X_test, y_test)
+
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=False,collate_fn=tokenizer_context.get_collate_fn(), sampler=sampler)
+test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False, collate_fn=tokenizer_context.get_collate_fn())
 
 
 
@@ -89,27 +105,38 @@ test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
 
 model_config = LSTMConfig(
-    vocab_size=codebert_tokenizer.tokenizer.vocab_size if tokenizer_context.strategy.provides_embeddings() else None,  
+    vocab_size= tokenizer_context.get_vocab_size(),
     embedding_dim=256, 
     hidden_dim=512,    
-    num_layers=1,      
-    dropout=0.2,       
-    max_seq_len=512,   
-    num_classes=4      
+    num_layers=3,      
+    dropout=0.5,       
+    max_seq_len=1028,   
+    num_classes=5     
 )
 model = LSTMMLCQ(model_config, tokenizer_context.strategy.provides_embeddings())
 model.to(device)
 
-optimizer_config = OptimizerConfig(optimizer_type="Adam", learning_rate=1e-4, weight_decay=0.01)
+optimizer_config = OptimizerConfig(optimizer_type="SGD", learning_rate=1e-4, momentum=0.9)
 scheduler_config = SchedulerConfig(scheduler_type="step", step_size=10, gamma=0.5)
 
 
 optimizer = get_optimizer(model, optimizer_config)
 scheduler = get_scheduler(optimizer, scheduler_config)
 
-training_config = TrainingConfig(batch_size=8, epochs=10, clip_grad=None)
+training_config = TrainingConfig(batch_size=16, epochs=10, clip_grad=1.0)
 
-criterion = nn.CrossEntropyLoss()
+criterion = nn.CrossEntropyLoss(weight=class_weights_tensor)
+
+
+#---------------------------- Training ---------------------------------------------------------------#
+
+logging.info("------------------------------------------------------------------------------")
+logging.info(f"Training Model with Config: {model_config}")
+logging.info(f"Optimizer Config: {optimizer_config}")
+logging.info(f"Scheduler Config: {scheduler_config}")
+logging.info(f"Training Config: {training_config}")
+
+
 
 def train_model(model, train_loader, optimizer, scheduler=None, training_config=None):
     model.train()
@@ -137,28 +164,18 @@ def train_model(model, train_loader, optimizer, scheduler=None, training_config=
                 nn.utils.clip_grad_norm_(model.parameters(), training_config.clip_grad)
             
 
-            if training_config.clip_grad is not None:
-                nn.utils.clip_grad_norm_(model.parameters(), training_config.clip_grad)
             
             optimizer.step()
 
             if scheduler is not None:
                 scheduler.step()
 
-            if scheduler is not None:
-                scheduler.step()
             
             running_loss += loss.item()
 
         epoch_time = time() - start_time
         print(f"Epoch [{epoch+1}/{training_config.epochs}], Loss: {running_loss/len(train_loader):.4f} | duration: {epoch_time:.3f}")
-
-
-train_model(model, train_loader, optimizer, scheduler, training_config)
-
-# -----------------------------------------------------------------------------------------------#
-# -----------------------------------------------------------------------------------------------#
-
+        logging.info(f"Epoch [{epoch+1}/{training_config.epochs}], Loss: {running_loss/len(train_loader):.4f} | duration: {epoch_time:.3f}")
 
 def evaluate_model(model, test_loader):
     model.eval()
@@ -174,19 +191,29 @@ def evaluate_model(model, test_loader):
 
             outputs = model(inputs)
             _, predicted = torch.max(outputs, 1)
-            y_true.extend(labels.cpu().numpy())
-            y_pred.extend(predicted.cpu().numpy())
+            y_true.extend(labels.cpu().tolist())
+            y_pred.extend(predicted.cpu().tolist())
     
     return y_true, y_pred
 
-y_true, y_pred = evaluate_model(model, test_loader)
-precision, recall, f1, _ = precision_recall_fscore_support(y_true, y_pred, average=None)
 
-for i, label in enumerate(label_encoder.classes_):  # label_encoder.classes_ contains the names of the smells
-    print(f"Class: {label}")
-    print(f"  Precision: {precision[i]:.4f}")
-    print(f"  Recall: {recall[i]:.4f}")
-    print(f"  F1-Score: {f1[i]:.4f}")
 
-print("\nClassification Report:")
-print(classification_report(y_true, y_pred, target_names=label_encoder.classes_))
+if __name__ == '__main__':
+
+    train_model(model, train_loader, optimizer, scheduler, training_config)
+
+    # -----------------------------------------------------------------------------------------------#
+
+
+    y_true, y_pred = evaluate_model(model, test_loader)
+    precision, recall, f1, _ = precision_recall_fscore_support(y_true, y_pred, average=None)
+
+    for i, label in enumerate(label_encoder.classes_):  # label_encoder.classes_ contains the names of the smells
+        print(f"Class: {label}")
+        print(f"  Precision: {precision[i]:.4f}")
+        print(f"  Recall: {recall[i]:.4f}")
+        print(f"  F1-Score: {f1[i]:.4f}")
+
+    print("\nClassification Report:")
+    print(classification_report(y_true, y_pred, target_names=label_encoder.classes_))
+    logging.info(classification_report(y_true, y_pred, target_names=label_encoder.classes_))
